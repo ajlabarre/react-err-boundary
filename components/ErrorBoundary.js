876f7148f9d91996ed1f5bfea2df249c1ad97b62
AJ LaBarre <aj@doctrin.se>
2021-03-19 17:58:50 +0100

    add code comments

--- components/ErrorBoundary.js
+++ components/ErrorBoundary.js
@@ -1,37 +1,57 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import clientLogger from '@common/utils/clientLogger';
 
+/**
+ * Error boundary component which catches errors below itself in the component tree
+ */
 class ErrorBoundary extends Component {
     static propTypes = {
         children: PropTypes.node,
         displayError: PropTypes.node,
         onError: PropTypes.func,
+		logger: PropTypes.object
     };
 
     static defaultProps = {
         onError: () => {},
     };
 
-	state = { hasError: false };
+	state = {
+		hasError: false
+	};
 
+	/**
+	 * Render a fallback UI after an error has been thrown
+	 * @param {*} error 
+	 * @returns 
+	 */
 	static getDerivedStateFromError(error) {
 		// Update state so the next render will show the fallback UI.
 		return { hasError: true };
 	}
 
+	/**
+	 * Log error information
+	 * @param {*} error 
+	 * @param {*} errorInfo 
+	 */
 	componentDidCatch(error, errorInfo) {
+		const { logger } = this.props;
 		let extra = {};
 		if ('componentStack' in errorInfo) {
 			extra = { componentStack: errorInfo.componentStack };
 		}
 		// You can also log the error to an error reporting service
-		clientLogger.error(error, extra);
+		logger.error(error, extra);
 
 		this.setState({ hasError: true });
 		this.props.onError(error);
 	}
 
+	/**
+	 * Render function which displays an error if present or returns children
+	 * @returns 
+	 */
 	render() {
 		const { displayError, children } = this.props;
 
