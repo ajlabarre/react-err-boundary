e2293a391a65b01d7c55b7d05c02af1557f2a5db
AJ LaBarre <aj@doctrin.se>
2021-03-19 18:12:44 +0100

    Error boundary repo setup (#1)

---
+++ components/ErrorBoundary.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Error boundary component which catches errors below itself in the component tree
+ */
+class ErrorBoundary extends Component {
+    static propTypes = {
+        children: PropTypes.node,
+        displayError: PropTypes.node,
+        onError: PropTypes.func,
+		logger: PropTypes.object
+    };
+
+    static defaultProps = {
+        onError: () => {},
+    };
+
+	state = {
+		hasError: false
+	};
+
+	/**
+	 * Render a fallback UI after an error has been thrown
+	 * @param {*} error 
+	 * @returns 
+	 */
+	static getDerivedStateFromError(error) {
+		// Update state so the next render will show the fallback UI.
+		return { hasError: true };
+	}
+
+	/**
+	 * Log error information
+	 * @param {*} error 
+	 * @param {*} errorInfo 
+	 */
+	componentDidCatch(error, errorInfo) {
+		const { logger } = this.props;
+		let extra = {};
+		if ('componentStack' in errorInfo) {
+			extra = { componentStack: errorInfo.componentStack };
+		}
+		// You can also log the error to an error reporting service
+		if (logger) logger.error(error, extra);
+
+		this.setState({ hasError: true });
+		this.props.onError(error);
+	}
+
+	/**
+	 * Render function which displays an error if present or returns children
+	 * @returns 
+	 */
+	render() {
+		const { displayError, children } = this.props;
+
+		if (this.state.hasError) {
+			if (displayError) return displayError;
+			// You can render any custom fallback UI
+			return null;
+		}
+
+		return children;
+	}
+}
+
+export default ErrorBoundary;
